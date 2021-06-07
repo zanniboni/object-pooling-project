5f8a1328e3cc1592142eb02d94b4be36d52eeb04
Vinicius Zaniboni Chagas <viny.zanchagas@gmail.com>
2021-06-07 10:49:33 -0300

    Update Projectile.cs
    Adicionado script do projetil

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Vector2 velocity = new Vector2(1.0f, 0.0f);
+    public SpriteRenderer renderer = null;
+    public float lifeTime = 3.0f;
+    private float deltaTime = 0.0f;
+
+    private void Awake()
     {
-        
+        renderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        ReturnToPooling();
     }
+
+    private void Update()
+    {
+        this.transform.Translate(velocity * Time.deltaTime);
+        deltaTime += Time.deltaTime;
+        if (deltaTime >= lifeTime)
+            ReturnToPooling();
+    }
+
+    public void Shoot(bool isRight)
+    {
+        renderer.flipX = !isRight;
+        if (!isRight)
+            velocity *= -1;
+    }
+
+    private void ReturnToPooling()
+    {
+        deltaTime = 0.0f;
+        velocity = new Vector2(6.0f, 0.0f);
+        renderer.flipX = false;
+        ObjectPooling.ReturnProjectile(this);
+    }
+
+
+
+
+
 }
