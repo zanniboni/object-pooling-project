ac2589ac474afb55dec2601a5bbf3de840126e00
Vinicius Zaniboni Chagas <viny.zanchagas@gmail.com>
2021-06-07 10:48:39 -0300

    Update ObjectPooling.cs
    Adicionado script de objectpooling

--- Assets/Scripts/ObjectPooling.cs
+++ Assets/Scripts/ObjectPooling.cs
@@ -2,17 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectPooling : MonoBehaviour
+[System.Serializable]
+public class ObjectPooling 
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject projectilePrefab = null;
+    static private ObjectPooling Instance = null;
+    public int length = 10;
+    private Projectile[] gamePooling = null;
+
+    public ObjectPooling()
     {
-        
+        Instance = this;        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnStart()
     {
-        
+        gamePooling = new Projectile[length];
+        for (int i = 0; i < length; i++)
+        {
+            GameObject projectile = GameObject.Instantiate<GameObject>(projectilePrefab);
+            gamePooling[i] = projectile.GetComponent<Projectile>();
+            gamePooling[i].gameObject.SetActive(false);
+        }
     }
+
+    public static Projectile GetProjectile()
+    {
+        if (Instance.gamePooling == null)
+            return null;
+        for (int i = 0; i < Instance.length; i++)
+        {
+            if(Instance.gamePooling[i].gameObject.activeSelf == false)
+            {
+                Instance.gamePooling[i].gameObject.SetActive(true);
+                return Instance.gamePooling[i];
+            }
+        }
+
+            return null;
+    }
+
+    public static void ReturnProjectile(Projectile projectile)
+    {
+        projectile.gameObject.SetActive(false);
+    }
+
 }
